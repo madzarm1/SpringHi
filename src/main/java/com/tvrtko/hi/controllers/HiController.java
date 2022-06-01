8cbdf3c16539719646e9c01c98a302717d8a9a62
Maksim <madzarmaksim@gmail.com>
2022-06-01 19:38:30 +0200

    task change

--- src/main/java/com/tvrtko/hi/controllers/HiController.java
+++ src/main/java/com/tvrtko/hi/controllers/HiController.java
@@ -1,14 +1,15 @@
 package com.tvrtko.hi.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HiController {
     
-    @GetMapping(value = "/hi")
-    public String SayHi() {
-        return ("Hi man/woman!");
+    @GetMapping(value = "/hey")
+    public String SayHi(@RequestParam String name) {
+        return ("Hi " + name);
     }
 
 }
